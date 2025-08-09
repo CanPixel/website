99decfd021e058e301e76cd005d632d9369f33b4
Can Ur (Cannemen) <Canur11@gmail.com>
2025-08-09 15:44:09 +0000

    final fix STABLE

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -62,9 +62,6 @@ export default function ContactPage() {
                 </a>
             ))}
         </div>
-        <div>
-            <ContactForm />
-        </div>
       </div>
     </div>
   );
