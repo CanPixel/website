9392c9339892f3696e0af586baed94cf0ebb4f05
Can Ur (Cannemen) <Canur11@gmail.com>
2025-08-13 10:05:28 +0000

    update projectcard orbitalresonance to a more fitting style, pastel red

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -112,6 +112,14 @@ export const projectStyles: { [id: string]: Partial<ProjectStyling> } = {
     borderColor: '#D203D2',
     animationClass: 'group-hover:shadow-[0_0_25px_5px_rgba(255,0,255,0.6)]',
   },
+  'orbital-resonance': {
+    backgroundColor: '#F8B4B4',
+    textColor: '#5B2C2C',
+    fontFamily: "'Roboto Mono', monospace",
+    backgroundImage: 'none',
+    borderColor: '#E47171',
+    animationClass: 'group-hover:shadow-[0_0_15px_3px_rgba(228,113,113,0.5)]',
+  },
   'ohmmylord': {
     backgroundColor: '#2c003e',
     textColor: '#00aaff',
@@ -193,15 +201,6 @@ export const projectStyles: { [id: string]: Partial<ProjectStyling> } = {
     borderColor: '#00D203',
     animationClass: 'group-hover:shadow-[0_0_25px_5px_rgba(255,0,255,0.6)]',
   },
-  'orbital-resonance': {
-    backgroundColor: '#2c003e',
-    textColor: '#00aaff',
-    fontFamily: 'spaceGrotesk',
-    backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.8) 100%), 
-                    url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23ff00ff' fill-opacity='0.1'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-    borderColor: '#00D203',
-    animationClass: 'group-hover:shadow-[0_0_25px_5px_rgba(255,0,255,0.6)]',
-  },
 };
 
 export const skillColors: { [key: string]: string } = {
@@ -264,3 +263,4 @@ export function getProjectStyling(projectId: string): ProjectStyling {
     // badgeBackgroundColor: projectSpecificStyling?.badgeBackgroundColor ?? defaultStyling.badgeBackgroundColor
   };
 }
+
