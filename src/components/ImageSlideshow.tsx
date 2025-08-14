3e1628ffc4f3c47a4e897a7f2140dd72b66f8995
Can Ur (Cannemen) <Canur11@gmail.com>
2025-08-14 12:49:50 +0000

    make the arrow buttons a light color. make images show their true size,

--- src/components/ImageSlideshow.tsx
+++ src/components/ImageSlideshow.tsx
@@ -10,6 +10,11 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 
 interface ImageSlideshowProps {
@@ -33,20 +38,35 @@ export function ImageSlideshow({ images }: ImageSlideshowProps) {
         {images.map((image, index) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
             <div className="p-1">
-                <div className="relative aspect-video rounded-lg overflow-hidden">
-                  <Image
-                    src={`/images/${image}`}
-                    alt={`Slideshow image ${index + 1}`}
-                    fill
-                    className="object-cover"
-                  />
-                </div>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <div className="relative aspect-video rounded-lg overflow-hidden cursor-pointer group">
+                    <Image
+                      src={`/images/${image}`}
+                      alt={`Slideshow thumbnail ${index + 1}`}
+                      fill
+                      className="object-cover transition-transform duration-300 group-hover:scale-105"
+                    />
+                    <div className="absolute inset-0 bg-black/20 group-hover:bg-black/40 transition-colors"></div>
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="max-w-4xl p-2">
+                  <div className="relative aspect-video">
+                    <Image
+                      src={`/images/${image}`}
+                      alt={`Fullscreen image ${index + 1}`}
+                      fill
+                      className="object-contain"
+                    />
+                  </div>
+                </DialogContent>
+              </Dialog>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <CarouselPrevious className="text-white hover:text-primary" />
+      <CarouselNext className="text-white hover:text-primary" />
     </Carousel>
   );
 }
